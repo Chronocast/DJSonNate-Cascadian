e3220c57beefe8555135f2d588c5a6d134f49683
Nate Hascup <nshascup@gmail.com>
2017-11-02 03:52:02 -0500

    changed admin>document button to a dropdown which will display a list of docs once doc table made in DB, also swapped construction and material icon for reasons noted in comments on admin page

--- tmp/2ylt7z1sgpess.2jn0gra06zy88.php
+++ tmp/2ylt7z1sgpess.2jn0gra06zy88.php
@@ -10,51 +10,60 @@
 	<!-- ACTIVE PROJECT START -->
 	<div id="active-div" class="portfolio container-fluid">
 		<div class="col-sm-12">
-			<h1>Active projects:</h1>
+			<h1>Active Projects:</h1>
 		</div>
 		
 		<!-- CARD DECK/COLUMNS START -->
 		<div class="card-deck">
 		  
+			<!-- repeat to display templated data -->  
 			<?php foreach (($projectDisplay?:[]) as $track): ?>
 		  
 				<!-- CARD -->
 				<div class="card">
 					<!-- CARD TITLE + OPTIONS BUTTONS-->
 					<!-- button class was btn-info -->
-					<button type="button" class="btn btn-title" data-toggle="modal" data-target="#exampleModal">
-						Project: <?= ($track['project_name'])."
+					<button type="button" class="btn btn-title " data-toggle="modal" data-target="#infoModal-<?= ($track['track_id']) ?>">
+						<?= ($track['project_name'])."
 " ?>
 					</button>
 					
 					<div class="d-inline-block text-center">
 						<a>
-							<button type="button" class="btn btn-warning" alt="documents" data-toggle="modal" data-target="#documentModal"> 
-								<i class="fa fa-file-text-o"></i> 
-							</button>
+							<div class="dropdown">
+								<!--<button type="button" class="btn btn-warning" alt="documents" data-toggle="modal" data-target="#documentModal">-->
+								<button type="button" title="Documents" class="btn btn-warning dropdown-toggle" alt="Documents"  id="docDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+									<i class="fa fa-file-text-o"></i> 
+								</button>
+								<div class="dropdown-menu" aria-labelledby="docDropdown">
+									<button class="dropdown-item" type="button" data-toggle="modal" data-target="#documentModal-<?= ($track['track_id']) ?>">Doc 1</button>
+									<button class="dropdown-item" type="button">Another action</button>
+									<button class="dropdown-item" type="button">Something else here</button>
+								</div>
+							</div>
 						</a>
 						<a>
-							<button type="button" class="btn btn-warning" alt="documents"> 
+							<button type="button"  title="Schedule" class="btn btn-warning" alt="Schedule"> 
 								<i class="fa fa-calendar-check-o"></i> 
 							</button>
 						</a>
 						<a>
-							<button type="button" class="btn btn-warning" alt="documents"> 
+							<button type="button"  title="Settings" class="btn btn-warning" alt="Settings"> 
 								<i class="fa fa-cogs"></i>
 							</button>
 						</a>
 						<a>
-							<button type="button" class="btn btn-warning" alt="documents"> 
+							<button type="button" title="Material" class="btn btn-warning" alt="Material"> 
 								<i class="fa fa-cubes"></i> 
 							</button>
 						</a>
 						<a>
-							<button type="button" class="btn btn-warning" alt="documents"> 
+							<button type="button" title="Punch List" class="btn btn-warning" alt="Punch List"> 
 								<i class="fa fa-list-ol"></i> 
 							</button>
 						</a>
 						<a>
-							<button type="button" class="btn btn-warning" alt="documents"> 
+							<button type="button" title="Approval" class="btn btn-warning" alt="Approval"> 
 								<i class="fa fa-check"></i> 
 							</button>
 						</a>
@@ -72,8 +81,8 @@
 					</div>
 				  
 				  
-					<!-- MODAL -->
-					<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+					<!-- Info MODAL -->
+					<div class="modal fade" id="infoModal-<?= ($track['track_id']) ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
 						<div class="modal-dialog" role="document">
 							<div class="modal-content">
 								<div class="modal-header">
@@ -89,6 +98,8 @@
 								</div>
 								<div class="modal-footer">
 									<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+									<button type="button" class="btn btn-primary">Edit</button>
+									<button type="button" class="btn btn-primary">Delete</button>
 									<button type="button" class="btn btn-primary">Save changes</button>
 								</div>
 							</div>
@@ -97,7 +108,7 @@
 					<!-- END Modal -->
 					
 					<!-- MODAL -->
-					<div class="modal fade" id="documentModal" tabindex="-1" role="dialog" aria-labelledby="documentModalLabel" aria-hidden="true">
+					<div class="modal fade" id="documentModal-<?= ($track['track_id']) ?>" tabindex="-1" role="dialog" aria-labelledby="documentModalLabel" aria-hidden="true">
 						<div class="modal-dialog" role="document">
 							<div class="modal-content">
 								<div class="modal-header">
@@ -113,20 +124,27 @@
 								</div>
 								<div class="modal-footer">
 									<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-									<button type="button" class="btn btn-primary">Save changes</button>
+									<button type="button" class="btn btn-success">Download</button>
+									<button type="button" class="btn btn-danger">Delete</button>
 								</div>
 							</div>
 						</div>
-					</div>
+						
 					<!-- END Modal -->
+					</div>
 				  
-				</div>
 				<!-- END Card -->
+				</div>
 		  
+			<!-- END repeat to display templated data -->
 			<?php endforeach; ?>
+			
+		<!-- CARD DECK/COLUMNS END -->
 		</div>
-	</div>
+		
 	<!-- ACTIVE PROJECT END -->
+	</div>
+	
 	
 	
 <?php echo $this->render('pages/admin-footer.html',NULL,get_defined_vars(),0); ?>
